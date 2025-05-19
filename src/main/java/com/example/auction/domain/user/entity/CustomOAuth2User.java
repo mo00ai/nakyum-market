f6c24e0e9512b478b2dcf7b63e0c24e6d0153cf4
escomputer <nujest307@naver.com>
2025-05-19 19:23:45 +0900

    feat : 소셜 로그인 사용자 (정보)

---
+++ src/main/java/com/example/auction/domain/user/entity/CustomOAuth2User.java
@@ -0,0 +1,52 @@
+package com.example.auction.domain.user.entity;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import lombok.Getter;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+/**
+ * 시큐리티 인증 후(소셜로그인 성공 후)에 사용자 정보 저장용
+ */
+@Getter
+public class CustomOAuth2User implements OAuth2User {
+	private final User user;
+	private final Map<String, Object> attributes;
+	private final String nameAttributeKey; //식별키 이메일이나 아이디
+	private final String jwtToken;
+
+	public CustomOAuth2User(User user, Map<String, Object> attributes, String nameAttributeKey, String jwtToken) {
+		this.user = user;
+		this.attributes = attributes;
+		this.nameAttributeKey = nameAttributeKey;
+		this.jwtToken = jwtToken;
+	}
+
+	@Override
+	public Map<String, Object> getAttributes() {
+		return attributes;
+	}
+
+	/**
+	 * 사용자의 권한 반환
+	 *
+	 */
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(new SimpleGrantedAuthority(user.getUserRole().name()));
+	}
+
+	/**
+	 * OAuth2에서 식별자로 사용하는 값 (ex: email 또는 provider id)
+	 */
+	@Override
+	public String getName() {
+		return String.valueOf(attributes.get(nameAttributeKey));
+	}
+
+}
