3e570524e4c0000b010e0755e9c67eee8ef36839
prem3dev <ecstasy1508@gmail.com>
2025-05-22 16:50:24 +0900

    feat: AuctionBidService 구현
    - Redis 및 DB 입찰 저장 로직 중개
    - 시작가 검증 로직 포함

---
+++ src/main/java/com/example/auction/domain/auctionbid/service/AuctionBidService.java
@@ -0,0 +1,42 @@
+package com.example.auction.domain.auctionbid.service;
+
+import java.time.LocalDateTime;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+
+import com.example.auction.common.exception.CustomException;
+import com.example.auction.common.exception.ErrorCode;
+import com.example.auction.domain.auctionbid.dto.BidRedisDto;
+import com.example.auction.domain.auctionbid.dto.request.BidRequestDto;
+import com.example.auction.domain.auctionbid.repository.AuctionBidRepository;
+import com.example.auction.domain.product.entity.Product;
+import com.example.auction.domain.product.repository.ProductRepository;
+
+@Service
+@RequiredArgsConstructor
+public class AuctionBidService {
+
+	private final AuctionBidRepository auctionBidRepository;
+	private final AuctionBidRedisService auctionBidRedisService;
+	private final ProductRepository productRepository;
+
+	public void bidSave(@NotNull Long productId, Long userId, @Valid BidRequestDto requestDto) {
+
+		Product product = productRepository.findById(productId).orElseThrow();
+		Long bidPrice = requestDto.getBidPrice();
+
+		if (bidPrice < product.getStartPrice()) {
+			throw new CustomException(ErrorCode.BID_PRICE_BELOW_START);
+		}
+
+		LocalDateTime now = LocalDateTime.now();
+
+		BidRedisDto dto = BidRedisDto.of(requestDto, product.getId(), userId, now);
+		auctionBidRedisService.trySaveHighestBid(productId, dto);
+	}
+}
