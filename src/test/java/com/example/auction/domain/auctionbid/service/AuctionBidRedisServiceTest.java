3a63ac6855390f4b044a99e5e3442e1c828325dc
prem3dev <ecstasy1508@gmail.com>
2025-05-22 23:41:39 +0900

    chore: 불필요한 어노테이션 제거
    - @ActiveProfiles는 사용되지 않아 제거하였습니다.

--- src/test/java/com/example/auction/domain/auctionbid/service/AuctionBidRedisServiceTest.java
+++ src/test/java/com/example/auction/domain/auctionbid/service/AuctionBidRedisServiceTest.java
@@ -16,14 +16,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import com.example.auction.common.exception.CustomException;
 import com.example.auction.common.exception.ErrorCode;
 import com.example.auction.domain.auctionbid.dto.BidRedisDto;
 import com.example.auction.domain.auctionbid.dto.request.BidRequestDto;
 
-@ActiveProfiles("test")
 @SpringBootTest
 public class AuctionBidRedisServiceTest {
 
